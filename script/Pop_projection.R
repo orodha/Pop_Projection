969e29b6010fc31c343c8626d20dd55421f233ad
Orodha <orodhairanzi@gmail.com>
2023-01-18 16:01:46 +0200

    Final Code

--- script/Pop_projection.R
+++ script/Pop_projection.R
@@ -6,10 +6,6 @@ library("tidyverse")
 library("readxl")
 library("janitor")
 
-# Working directory
-#getwd()
-#setwd("C:/Users/ENVYLL/Downloads")
-
 # Reading excel Rwanda Population data
 pop_rwanda <- readxl::read_excel("pop_data/pop_projection_data.xlsx", col_names=TRUE, .name_repair = "minimal")
 
@@ -19,19 +15,18 @@ year <- pop_rwanda %>%
           drop_na()%>%
           select(Year, '5 year age group', Population)
 
-
 #Create long data frame with sex, population and age group
-
 sex <- pop_rwanda %>%
           row_to_names(row_number=1, remove_row = TRUE)
+
 #change NA column name
 colnames(sex)[1]  <- '5 year age group'
+
 #make data long
 sex <- sex %>%
-      pivot_longer(cols = !'5 year age group', names_to="Sex", values_to = "Population")%>%
+      pivot_longer(cols = !'5 year age group', names_to="Sex", values_to = "Population") %>% 
       select('5 year age group', Population, Sex)
 
 #Combine data frames using population and age group as an index
 combined <- sex %>%
         inner_join(year, by = c('5 year age group', 'Population'))
-
